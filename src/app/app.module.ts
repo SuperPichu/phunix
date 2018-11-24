1f068a83319868789cbd8a307e448945e921adcf
Ian Walton <iwalton3@gmail.com>
2018-11-24 18:36:55 -0500

    Create example menu and fill page with interface.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,17 +4,21 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { RouterModule } from '@angular/router';
-import { MatSidenavModule, MatToolbarModule, MatListModule } from '@angular/material';
+import { MatSidenavModule, MatToolbarModule, MatListModule, MatButtonModule, MatIconModule } from '@angular/material';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { PracticeSettingsComponent } from './practice-settings/practice-settings.component';
 
 const appRoutes = [
-  { path: 'home', component: HomeComponent, data: {title: 'Phunix'} },
+  { path: 'home', component: HomeComponent },
+  { path: 'practice_settings', component: PracticeSettingsComponent },
   { path: '', redirectTo: '/home', pathMatch: 'full' },
 ];
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    PracticeSettingsComponent
   ],
   imports: [
     BrowserModule,
@@ -23,15 +27,21 @@ const appRoutes = [
     ),
     MatSidenavModule,
     MatToolbarModule,
-    MatListModule
+    MatListModule,
+    MatButtonModule,
+    MatIconModule,
+    BrowserAnimationsModule
   ],
   exports: [
     MatSidenavModule,
     MatToolbarModule,
     MatListModule,
-    RouterModule
+    MatButtonModule,
+    MatIconModule,
+    RouterModule,
+    BrowserAnimationsModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
 export class AppModule { }
---
+++ src/app/practice-settings/practice-settings.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-practice-settings',
+  templateUrl: './practice-settings.component.html',
+  styleUrls: ['./practice-settings.component.css']
+})
+export class PracticeSettingsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
