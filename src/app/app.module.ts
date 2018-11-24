07d38c9c3db257ba1c8d1b54a6dd490638acc4c6
Ian Walton <iwalton3@gmail.com>
2018-11-24 13:33:33 -0500

    Start working on Material and Routing. (Unbuildable)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,34 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { RouterModule } from '@angular/router';
+import { MatSidenavModule, MatToolbarModule, MatListModule } from '@angular/material';
+
+const appRoutes = [
+  { path: 'home', component: HomeComponent, data: {title: 'Phunix'} },
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(
+      appRoutes
+    ),
+    MatSidenavModule,
+    MatToolbarModule,
+    MatListModule
+  ],
+  exports: [
+    MatSidenavModule,
+    MatToolbarModule,
+    MatListModule,
+    RouterModule
   ],
   providers: [],
   bootstrap: [AppComponent]
