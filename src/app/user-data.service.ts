091465757e69f8571afbc0ea7ee9ad9ae849e2b9
Ian Walton <iwalton3@gmail.com>
2018-11-25 00:11:06 -0500

    Implement hard-coded data to demo services.

--- src/app/user-data.service.ts
+++ src/app/user-data.service.ts
@@ -6,4 +6,12 @@ import { Injectable } from '@angular/core';
 export class UserDataService {
 
   constructor() { }
+  getFeedback() {
+    // In the future this won't be hard-coded.
+    return {
+      'completed': 23,
+      'correct': 15,
+      'time': 1650
+    };
+  }
 }
