144ec5b31a8504f96af833b2bcd8e731315fec4e
Ian Walton <iwalton3@gmail.com>
2018-11-28 14:48:45 -0500

    Add iOS icon support.

---
+++ src/assets/icons/gen-icons-ios.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+convert icon-512x512.png -resize 57x57 icon-57x57.png
+convert icon-512x512.png -resize 72x72 icon-72x72.png
+convert icon-512x512.png -resize 114x114 icon-114x114.png
+
+
